a18b8499a2a71d88327468c721182d6a57f9ff75
raebaxter <betsystevens@comcast.net>
2019-06-29 23:08:43 -0500

    sample DatabaseConnection

--- includes/YourDatabaseConnection.php
+++ includes/YourDatabaseConnection.php
@@ -1,4 +1,9 @@
-<?php	
+<?php
+/*
+ *  change this file name to DatabaseConnection.php 
+ *  use your host, dbname, user and password
+ *
+ */
 $pdo = new PDO('mysql:host=localhost;dbname=flowers;charset=utf8',
 	'user','password');
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
